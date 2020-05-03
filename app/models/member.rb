76801791b33d4f20356572f0fd6407dded0963b7
Charles Butler <cdbutle@outlook.com>
2020-05-03 04:28:24 +0000

    Automatically backed up by Learn

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,5 +1,21 @@
 
-class Member 
+class Member
+  
+ attr_reader :name, :power, :bio
+
+ members = []
+
+ def initialize(args)
+   @name = args[:name]
+   @power = args[:power]
+   @bio = args[:bio]
+   COURSES << self
+ end
+
+ def self.all
+   members
+ end
+
 
 
 end
