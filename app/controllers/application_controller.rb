cee0293db0b4e9f1db7adbb116ee94478094c8c7
Charles Butler <cdbutle@outlook.com>
2020-05-03 03:38:13 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,19 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :create
+    end
 
+    post '/team' do
+    params[:team][:member].each do |member_data|
+      Member.new(member_data)
+    end
+
+    @team = Team.new(params[:team])
+
+    @members = members.all
+
+    erb :team
+  end
 end
